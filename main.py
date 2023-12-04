f1545ca1054ff6f85865d3e9f7ee763246f35ad1
VohnikMB <101456452+VohnikMB@users.noreply.github.com>
2023-12-04 15:49:53 +0200

    New Start

---
+++ main.py
@@ -0,0 +1,30 @@
+# main.py
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from UI.AuthenticationUI.authenticationUI import Ui_MainWindow
+from UI.AuthenticationUI.profileEmployeeUI import ProfileWindow
+from BLL.Authentication.authentication import check_credentials
+from BLL.Authorization.profilSQL import get_employee_info as profileSQL
+
+
+class LoginWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.login_function)
+
+    def login_function(self):
+        if check_credentials(self.login_label.text(), self.password_label.text()):
+            user_name, work_inf = profileSQL(self.login_label.text(), self.password_label.text())
+            self.profile_window = ProfileWindow(user_name, work_inf)
+            self.profile_window.show()
+            self.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    login_window = LoginWindow()
+    login_window.show()
+
+    sys.exit(app.exec_())
